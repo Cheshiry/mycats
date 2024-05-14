061c4132573fb18f3b5cef583d110036ebc91b56
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-05-14 16:04:59 -0300

    Update javascript.js
    Aula 05 parte1

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
-//02
-window.alert('hi');
-window.confirm('Estágostandodocurso?');
-window.prompt(' Qual o seu nome?');
+//Curso em Vídeo 
+//exercício001
+
+/*
+ window.alert('hi');
+ window.confirm('Estágostandodocurso?');
+*/
+
+//exercício002
+
+var nome = window.prompt(' Qual o seu nome?');
+window.alert('Seja bem-vindo'+' '+nome);
