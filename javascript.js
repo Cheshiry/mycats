d1f3f976c65cc046865523c10c6d33ad21181668
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-05-14 21:48:53 -0300

    Update javascript.js
    Testes number

--- javascript.js
+++ javascript.js
@@ -7,13 +7,13 @@
 */
 
 //exercício002
-
+/*
 var nome = window.prompt(' Qual o seu nome?');
 window.alert('Seja bem-vindo'+' '+nome);
-
+*/
 var n1 = Number(window.prompt('Digite um número:'));
 var n2 = Number(window.prompt('Digite um número:'));
 var soma = n1+n2;
 
-window.alert(soma);
+window.alert(soma.number);
 //document.getElementById('container') = soma;
